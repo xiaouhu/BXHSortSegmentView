b143ccd769e685da0e7e0f5f1cfba88b22d09b0f
xiaouhu <542601704@qq.com>
2017-11-02 14:42:03 +0800

    Initial Commit

---
+++ BXHSortSegmentView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BXHSortSegmentView
+//
+//  Created by 步晓虎 on 2017/11/2.
+//  Copyright © 2017年 步晓虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BXHSortSegmentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BXHSortSegmentView
+//
+//  Created by 步晓虎 on 2017/11/2.
+//  Copyright © 2017年 步晓虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
